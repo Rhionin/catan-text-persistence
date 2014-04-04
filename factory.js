9c48d3256ff407e0954252be37a74c8a26e12f70
Tyson Decker <tyson.decker@gmail.com>
2014-04-04 01:49:03 -0600

    Adding yui documentation for the factory

--- factory.js
+++ factory.js
@@ -8,14 +8,57 @@ var gameStore = new game.TextGameStore();
 var userStore = new user.TextUserStore();
 
 _.extend(exports, {
+	/**
+	 * [returns an instance of the text persistence command DAO]
+	 * <pre>
+
+	    Domain: None
+	      
+	    Invariants:
+	        INVARIANT: None
+	    
+	    	PRE: None
+	    	POST: a valid commandStore is returned
+	    </pre>
+	 * @method getCommandStore
+	 * @return {TextCommandStore}
+	 */
 	getCommandStore: function()
 	{
 		return commandStore;
 	},
+	/**
+	 * [returns an instance of the text persistence game DAO]
+	 *
+	 * 	    Domain: None
+	      
+	    Invariants:
+	        INVARIANT: None
+	    
+	    	PRE: None
+	    	POST: a valid gameStore is returned
+	    </pre>
+	 * @method getGameStore
+	 * @return {TextGameStore}
+	 */
 	getGameStore: function()
 	{
 		return gameStore;
 	},
+	/**
+	 * [returns an instance of the text persistence user DAO]
+	 *
+	 * 	    Domain: None
+	      
+	    Invariants:
+	        INVARIANT: None
+	    
+	    	PRE: None
+	    	POST: a valid userStore is returned
+	    </pre>
+	 * @method getUserStore
+	 * @return {TextUserStore}
+	 */
 	getUserStore: function()
 	{
 		return userStore;
