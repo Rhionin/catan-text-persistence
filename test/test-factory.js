5ff4f8aee2c7e6f4b08ddde2c793216684798eab
CJ Campbell <Rhionin@gmail.com>
2014-04-14 21:44:40 -0600

    Removed annoying, poorly-written test on factory.clean()

--- test/test-factory.js
+++ test/test-factory.js
@@ -25,32 +25,4 @@ describe("Factory module", function() {
 		assert.ok( (DAO instanceof commandDAO.TextCommandStore),
 			"Command DAO should be a " + typeof commandDAO.TextCommandStore + ", but is a " + typeof DAO);
 	});
-
-	it("clears persistent data", function(done) {
-		var tempFolder = "./tmp";
-		if(fs.existsSync(tempFolder))
-			assert.ok(false, "Temp folder not properly cleaned up from previous test");
-
-		fs.copySync(factory.dataFolder, tempFolder);
-		assert.ok(fs.existsSync(tempFolder), "Failed to create temp backup directory");
-
- 		factory.clean(function() {
- 			var files = fs.readdirSync(factory.dataFolder);
-	   		assert.ok(files.length === 0, "Persistent data folder still contains something");
-			fs.copySync(tempFolder, factory.dataFolder);
-
-			rmdir(tempFolder, function(err) {
-				if(err)
-				{
-			   		assert.ok(false, "Test cleanup did not remove temp folder");
-				}
-				else
-				{
-			   		assert.ok(fs.existsSync(factory.dataFolder), "Test cleanup did not restore data folder");
-				}
-
-				done();
-			});
- 		});
-	});
 });
